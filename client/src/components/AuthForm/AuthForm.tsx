bf95b8433ef04acc71b8494a7ef7708f254cfe1f
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-10-08 18:50:42 +0100

    Get inputs to animate their labels

--- client/src/components/AuthForm/AuthForm.tsx
+++ client/src/components/AuthForm/AuthForm.tsx
@@ -1,7 +1,13 @@
+'use client';
+import { useState } from 'react';
 import styles from './AuthForm.module.scss';
 
-// AuthForm.js
 const AuthForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameFocused, setUsernameFocused] = useState(false);
+  const [passwordFocused, setPasswordFocused] = useState(false);
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="">
@@ -11,20 +17,44 @@ const AuthForm = () => {
       </div>
       <form className="flex flex-col w-full max-w-md mx-auto my-auto pb-4">
         <div className="px-8 mt-4 flex-1 flex flex-col justify-center">
-          <div className="mb-8">
-            <label htmlFor="" className="block mb-2">
+          <div
+            className={`relative mb-12 ${
+              usernameFocused || username ? styles.focused : ''
+            }`}>
+            <input
+              type="text"
+              className={styles.inputLine}
+              placeholder={usernameFocused || username ? '' : 'Username'}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              onFocus={() => setUsernameFocused(true)}
+              onBlur={() => setUsernameFocused(false)}
+            />
+            <label htmlFor="" className={styles.label}>
               Username
             </label>
-            <input type="text" className={styles.inputLine} />
           </div>
-          <div className="mb-12">
-            <label htmlFor="" className="block mb-2">
+          <div
+            className={`relative mb-12 ${
+              passwordFocused || password ? styles.focused : ''
+            }`}>
+            <input
+              type="password"
+              className={styles.inputLine}
+              placeholder={passwordFocused || password ? '' : 'Password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={() => setPasswordFocused(true)}
+              onBlur={() => setPasswordFocused(false)}
+            />
+            <label htmlFor="" className={styles.label}>
               Password
             </label>
-            <input type="password" className={styles.inputLine} />
           </div>
           <div className="flex justify-center">
-            <button className={`${styles.button} py-4 px-12 text-xl`}>Sign in</button>
+            <button className={`${styles.button} py-4 px-12 text-xl`}>
+              Sign in
+            </button>
           </div>
         </div>
       </form>
